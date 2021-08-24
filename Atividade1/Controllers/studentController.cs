d16e79d1c2cf808d5dc99fd8782988b7c8e6f783
VarRgas <89429606+VarRgas@users.noreply.github.com>
2021-08-24 10:32:22 -0300

    Atividade uniftec

---
+++ Atividade1/Controllers/studentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectFacul.Controllers
+{
+    public class studentController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Listar() => View();
+
+        public IActionResult Gravar() => View();
+
+        public IActionResult Excluir() => View();
+
+        public IActionResult Procurar() => View();
+    }
+}
---
+++ Atividade1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectFacul.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
